44f19feb74c490e1bec847f261f831a6caf98097
bernardoras <bernardoras@ufmg.br>
2025-11-06 19:15:01 -0300

    K8s configuration files for job (ML model generator) and configmap (dataset configuration). PVC just for doc.

---
+++ run.ps1
@@ -0,0 +1,5 @@
+git add *
+git commit -m "K8s configuration files for job (ML model generator) and configmap (dataset configuration). PVC just for doc."
+git branch -M main
+git remote add origin https://github.com/bernardoras/comp_nuvem_tp2.git
+git push -u origin main
